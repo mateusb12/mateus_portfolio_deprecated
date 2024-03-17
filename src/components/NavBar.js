d8485256b5b05b6874fb93989419fd9974e7b73d
Mateus <mateus_b09@hotmail.com>
2024-03-17 16:39:05 -0300

    compile update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,20 +44,24 @@ export const NavBar = () => {
                     </Navbar.Toggle>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                            <Nav.Link href="#home"
+                                      className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
+                                      onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                            <Nav.Link href="#skills"
+                                      className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+                                      onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                            <Nav.Link href="#projects"
+                                      className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
+                                      onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                            <div className="social-icon">
+                                <button style={{background: 'none', border: 'none'}} onClick={() => {}}><img src={navIcon1} alt=""/></button>
+                                <button style={{background: 'none', border: 'none'}} onClick={() => {}}><img src={navIcon2} alt=""/></button>
+                                <button style={{background: 'none', border: 'none'}} onClick={() => {}}><img src={navIcon3} alt=""/></button>
+                            </div>
+                            <HashLink to='#connect'>
+                                <button className="vvd"><span>Let’s Connect</span></button>
+                            </HashLink>
                         </Nav>
-                        <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
